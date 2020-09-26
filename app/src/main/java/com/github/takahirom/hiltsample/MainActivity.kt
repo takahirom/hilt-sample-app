8c36602aaa4e27d8f10c81e2808f0ff452f1c8a4
takahirom <takam.dev@gmail.com>
2020-09-26 20:46:36 +0900

    first commit

---
+++ app/src/main/java/com/github/takahirom/hiltsample/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.github.takahirom.hiltsample
+
+import android.app.Application
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.*
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val videoPlayer = VideoPlayer()
+        videoPlayer.play()
+    }
+}
+
+class VideoPlayer {
+    private val database = Room
+        .databaseBuilder(
+            App.instance,
+            VideoDatabase::class.java,
+            "database"
+        )
+        .createFromAsset("videos.db")
+        .build()
+    private val codecs = listOf(FMP4, WebM, MPEG_TS, AV1)
+    private var isPlaying = false
+
+    fun play() {
+        isPlaying = true
+//        video?.url
+//        codecs.forEach {
+//        }
+        // ...
+    }
+}
+
+interface Codec
+object FMP4 : Codec
+object WebM : Codec
+object MPEG_TS : Codec
+object AV1 : Codec
+
+@Database(entities = arrayOf(Video::class), version = 1)
+abstract class VideoDatabase : RoomDatabase() {
+    abstract fun videoDao(): VideoDao
+}
+
+@Dao
+interface VideoDao {
+    @Query("SELECT * FROM video")
+    fun getAll(): List<Video>
+}
+
+
+@Entity
+data class Video(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "manifestUrl") val url: String?
+)
