c85a6f668a0bf447c0a4b119f4f6d8cc8c2cff80
takahirom <takam.dev@gmail.com>
2020-10-03 12:29:57 +0900

    Add DataModule for injecting database

--- app/src/main/java/com/github/takahirom/hiltsample/MainActivity.kt
+++ app/src/main/java/com/github/takahirom/hiltsample/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.github.takahirom.hiltsample
 
 import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -31,7 +37,9 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class VideoPlayer @Inject constructor() {
+class VideoPlayer @Inject constructor(
+    private val database: VideoDatabase
+) {
     private var isPlaying = false
 
     fun play() {
@@ -67,3 +75,19 @@ data class Video(
     @ColumnInfo(name = "title") val title: String?,
     @ColumnInfo(name = "manifestUrl") val url: String?
 )
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+    @Provides
+    fun provideVideoDB(@ApplicationContext context: Context): VideoDatabase {
+        return Room
+            .databaseBuilder(
+                context,
+                VideoDatabase::class.java,
+                "database"
+            )
+            .createFromAsset("videos.db")
+            .build()
+    }
+}
